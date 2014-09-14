21ea7ca991ade69001b8e3d39b9b08bee970583d
Clashsoft <clashsoft@hotmail.com>
2014-09-14 18:12:05 +0200

    Added interface for @Agadar's Brewing API

---
+++ src/main/java/com/agadar/brewingapi/BrewingRecipes.java
@@ -0,0 +1,111 @@
+package com.agadar.brewingapi;
+
+import java.util.List;
+
+import clashsoft.brewingapi.potion.PotionTypeList;
+import clashsoft.brewingapi.potion.recipe.CustomPotionRecipe;
+import clashsoft.brewingapi.potion.recipe.PotionRecipes;
+import clashsoft.brewingapi.potion.type.IPotionType;
+import clashsoft.brewingapi.potion.type.PotionType;
+import clashsoft.cslib.logging.CSLog;
+
+import net.minecraft.item.ItemPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.potion.PotionEffect;
+
+/**
+ * Manages all brewing recipes.
+ */
+public class BrewingRecipes
+{
+	private static final BrewingRecipes	brewingBase	= new BrewingRecipes();
+	
+	/**
+	 * Returns the instance.
+	 */
+	public static BrewingRecipes brewing()
+	{
+		return brewingBase;
+	}
+	
+	/**
+	 * Adds a new brewing recipe, where applying the ingredient to the input
+	 * results in the output.
+	 * 
+	 * @param input
+	 *            the input potion
+	 * @param ingredient
+	 *            the ingredient
+	 * @param output
+	 *            the output potion
+	 */
+	public void addBrewing(ItemStack input, ItemStack ingredient, ItemStack output)
+	{
+		if (input == null || input.stackSize <= 0 || ingredient == null || ingredient.stackSize <= 0 || output == null || output.stackSize <= 0)
+		{
+			CSLog.error("Error while adding a brewing recipe - the ItemStacks may not be null or have a stack size smaller than 1.");
+			return;
+		}
+		
+		if (!(input.getItem() instanceof ItemPotion) || !(output.getItem() instanceof ItemPotion))
+		{
+			CSLog.error("Error while adding a brewing recipe - the Items of the input and the output ItemStacks have to be instances of ItemPotion.");
+			return;
+		}
+		
+		new CustomPotionRecipe(input, ingredient, output).register();
+	}
+	
+	/**
+	 * Returns whether the given {@link ItemStack} is a valid ingredient for any
+	 * brewing recipe.
+	 * 
+	 * @param ingredient
+	 *            the ingredient
+	 */
+	public boolean isPotionIngredient(ItemStack ingredient)
+	{
+		return PotionRecipes.isIngredient(ingredient);
+	}
+	
+	/**
+	 * Returns the result of applying the given ingredient to the given input.
+	 * Returns null if the brewing recipe does not exist.
+	 * 
+	 * @param input
+	 *            the input potion
+	 * @param ingredient
+	 *            the ingredient
+	 */
+	public ItemStack getBrewingResult(ItemStack input, ItemStack ingredient)
+	{
+		return PotionRecipes.applyIngredient(input, ingredient);
+	}
+	
+	/**
+	 * Translates the given {@link List} of {@link PotionEffect}s to an
+	 * {@link NBTTagList} and adds it to the given {@link ItemStack}'s NBT.
+	 */
+	public void setEffects(ItemStack stack, List<PotionEffect> effects)
+	{
+		PotionTypeList potionTypes = new PotionTypeList(stack);
+		for (PotionEffect effect : effects)
+		{
+			IPotionType type = PotionType.getFromEffect(effect);
+			potionTypes.add(type);
+		}
+		potionTypes.save();
+	}
+	
+	/**
+	 * Calculates the duration modifier for a potion effect according to the
+	 * given parameters.
+	 */
+	public float getDurationModifier(boolean splash, int amplification, boolean extended)
+	{
+		float modifier = splash ? 0.75F : 1.0F;
+		modifier /= 2 << amplification;
+		return extended ? modifier * 8F / 3F : modifier;
+	}
+}
